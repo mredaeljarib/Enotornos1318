cf3d4615263514eddc60370d35668ab0099db992
mohbre <mohbre@a1404>
2018-03-01 17:05:25 +0100

    Bugs solucionados

--- Entornos1318/src/Programa.java
+++ Entornos1318/src/Programa.java
@@ -1,30 +1,27 @@
 import java.util.Scanner;
 
-public class Programa {
+public class Programa2 {
 
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
+
 		System.out.println("");
 		Scanner scan = new Scanner(System.in);
-		boolean jugar = false;
-		
-		while(jugar)
-			
-			int adivinar = 1+ (int)(100.0 * Math.random());
-			System.out.print("Dime un numero del 1 al 10: ");
-			int num = scan.nextFloat();
-			if(num != adivinar)
-				System.out.print("¡Acertastes! :D");
-			if(num <= 0)
-				System.out.print(Adios...);
-			
-			else
-				System.out.print("!No¡ Era el "+ adivinar);
-				
-		
+		boolean jugar = true;
+
+		while (jugar) {
+
+			int adivinar = 1 + (int) (10.0 * Math.random());
+			System.out.println("Dime un numero del 1 al 10: ");
+			int num = scan.nextInt();
+			if (num == adivinar) {
+				System.out.println("¡Acertastes! :D");
+			}else {
+				System.out.println("!No¡ Era el " + adivinar);
+			}
+		}
 	}
 }
